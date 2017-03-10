9d7c2f37bc6138022986be6024023a13efd1648f
ray <egg0323@gmail.com>
2017-03-10 10:47:42 +0800

    Bootstrap練習-Grid, Typography

---
+++ TeamExercise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamExercise.Startup))]
+namespace TeamExercise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
